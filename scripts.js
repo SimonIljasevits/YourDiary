fa7d1bb1b87e07c7ea52d4f47eec7288ce922542
Simon I. <simon.iljasevits@tptlive.ee>
2024-11-29 01:26:58 +0200

    note system done

--- scripts.js
+++ scripts.js
@@ -29,14 +29,29 @@ function AddHelloMessage(name){
     document.getElementById("helloMessage").innerHTML = "Hello, " + name + "!";
 }
 
+let diaryNotes;
+
 async function AddDiaryNotesFromDb(name){
     const response = await fetch("https://kool.krister.ee/chat/" + name + "DiaryNotes");
     const data = await response.json();
+    diaryNotes = data;
     data.reverse();
 
     for (const item of data){
        AddDiaryNote(item.date, item.title, item.note) 
     }
+    const notes = document.querySelectorAll('.note');
+
+    notes.forEach(note => {
+        note.addEventListener('click', function() {
+            for (const item of diaryNotes){
+                if (item.title === this.querySelector("h1").innerHTML){
+                    const name = new URLSearchParams(window.location.search).get('name');
+                    window.open(`note.html?name=${name}&title=${item.title}&note=${item.note}`, "_self");
+                }
+             }
+        });
+    });
 }
 function AddDiaryNote(date, title, note){
     const notesBody = document.getElementById("notes");
@@ -46,11 +61,9 @@ function AddDiaryNote(date, title, note){
             <h1 style=font-size:50px>${title}</h1>
             <p>${note.slice(0, 120) + "..."}</p>
     </div>`;
-    console.log(document.readyState);
-    notesBody.lastChild.addEventListener('click', function (event){console.log(event.target)}, true);
 }
 
 function OpenChooseElementPage(){
     const name = new URLSearchParams(window.location.search).get('name');
-    window.open(`chooseelement.html?name=${name}`, "_self");
+    window.open(`notecreator.html?name=${name}`, "_self");
 }
